c0f3654b519297dadb2ce9ed41fd146d43cb8075
harold1307 <electronicsistems15@gmail.com>
2022-08-16 21:34:37 -0400

    Semana3-dia1-6

--- configuracion/axios.js
+++ configuracion/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const clienteAxios = axios.create({
-    baseURL: 'https://directorioseguridad.dte.gt/api/',        
+    baseURL: 'https://directorioseguridad.dte.gt/api/',    
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    withCredentials: true    
 });
 
 export default  clienteAxios;
