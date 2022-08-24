55cd6147e071b2d33b6796094dcdb7c78c43cc5a
harold1307 <electronicsistems15@gmail.com>
2022-08-24 00:02:28 -0400

    Semana4-dia1-2

--- pages/dashboard/empresa/crear/CrearEmpresa.jsx
+++ pages/dashboard/empresa/crear/CrearEmpresa.jsx
@@ -1,17 +1,10 @@
 import React , {useState} from 'react';
 import { useForm } from 'react-hook-form';
-import { CountryDropdown, RegionDropdown, CountryRegionData } from 'react-country-region-selector';
 import {  Col, Row  } from 'antd';
 
 export default function CrearEmpresa() {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = data => console.log(data);
-
-  const { region, setRegion } = useState({});
-
-  const selectRegion = ({value})=>{
-      console.log(value)
-  }
 
   return (
     <Row className='bg-white my-5 py-5'>
